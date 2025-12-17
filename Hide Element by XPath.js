42adf46780559e0533d6e8459d426b6c3068c47a
Florin <hey@florincatalin.com>
2025-12-17 09:35:01 -0800

    Brute

--- Hide Element by XPath.js
+++ Hide Element by XPath.js
@@ -38,19 +38,20 @@
   hideTarget("//html/body/div/div/div[2]/div[2]/div/div");
 
   // 2. OBSERVER: Watch for changes in the page (dynamic loading)
-  // const observer = new MutationObserver((mutations) => {
+  const observer = new MutationObserver((mutations) => {
     // For performance, we only re-check if nodes were actually added
-    // let shouldCheck = false;
-    // for (const mutation of mutations) {
-    //   if (mutation.addedNodes.length > 0) {
-    //     shouldCheck = true;
-    //     break;
-    //   }
-    // }
+    let shouldCheck = false;
+    for (const mutation of mutations) {
+      if (mutation.addedNodes.length > 0) {
+        shouldCheck = true;
+        break;
+      }
+    }
 
-    // if (shouldCheck) {
-    //   hideTarget();
-    // }
+    if (shouldCheck) {
+    hideTarget("//html/body/div/div/div[2]/div");
+    hideTarget("//html/body/div/div/div[2]/div[2]/div/div");
+    }
   });
 
   // Start observing the document body for added nodes
